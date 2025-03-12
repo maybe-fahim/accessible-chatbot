879c59fb537af5f2b3ce8f0d288bd3acbe51ad95
Fahim <139593307+maybe-fahim@users.noreply.github.com>
2025-03-12 15:26:16 +0000

    Initial commit

---
+++ chatbot/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="min-h-screen bg-blue-600 text-white flex items-center justify-center">
+      <h1 className="text-4xl font-bold underline">Tailwind is working!</h1>
+    </div>
+  );
+}
+
+export default App
